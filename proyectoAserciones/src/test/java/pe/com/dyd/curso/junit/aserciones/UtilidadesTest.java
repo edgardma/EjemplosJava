997a4db149df4b1d52da4a931e9afb0e54fa5379
Edgard Marquez <edgardma@gmail.com>
2018-12-29 22:43:41 -0500

    Modificaciones

--- proyectoAserciones/src/test/java/pe/com/dyd/curso/junit/aserciones/UtilidadesTest.java
+++ proyectoAserciones/src/test/java/pe/com/dyd/curso/junit/aserciones/UtilidadesTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
