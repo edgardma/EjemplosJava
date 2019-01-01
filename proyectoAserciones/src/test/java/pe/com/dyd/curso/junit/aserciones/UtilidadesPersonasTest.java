6e9d14224de957d85e42526f10a8b2a67a299388
Edgard Marquez <edgardma@gmail.com>
2019-01-01 09:08:13 -0500

    Orden de los test (no se recomienda ordenar los test, estos deben ser independientes)

--- proyectoAserciones/src/test/java/pe/com/dyd/curso/junit/aserciones/UtilidadesPersonasTest.java
+++ proyectoAserciones/src/test/java/pe/com/dyd/curso/junit/aserciones/UtilidadesPersonasTest.java
@@ -4,8 +4,11 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UtilidadesPersonasTest {
 	
 	private UtilidadesPersonas utils;
