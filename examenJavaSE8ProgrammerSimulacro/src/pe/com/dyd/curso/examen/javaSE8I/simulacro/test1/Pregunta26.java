3c05b4b71611d7422470af13a4ea6bc9bc626c59
Edgard Marquez <edgardma@gmail.com>
2019-02-19 06:02:21 -0500

    Nuevas preguntas de ejemplo para el examen 8

---
+++ examenJavaSE8ProgrammerSimulacro/src/pe/com/dyd/curso/examen/javaSE8I/simulacro/test1/Pregunta26.java
@@ -0,0 +1,14 @@
+package pe.com.dyd.curso.examen.javaSE8I.simulacro.test1;
+
+public class Pregunta26 {
+	public static void main(String[] args) {
+		int x = 200;
+		System.out.println(m1(x));
+		System.out.println(" " + x);
+	}
+
+	public static int m1(int x) {
+		x = x * 2;
+		return x;
+	}
+}
