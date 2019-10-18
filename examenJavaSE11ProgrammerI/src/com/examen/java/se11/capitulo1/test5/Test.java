b47def5948ec60ca700afb7e5190148b0ae6ad2d
Edgar M. Marquez Aylas <edgardma@gmail.com>
2019-10-17 22:47:38 -0500

    Test 5

---
+++ examenJavaSE11ProgrammerI/src/com/examen/java/se11/capitulo1/test5/Test.java
@@ -0,0 +1,11 @@
+package com.examen.java.se11.capitulo1.test5;
+
+public class Test {
+	public static void main(String[] args) {
+		String javaworld = "JavaWorld";
+		String java = "Java";
+		String world = "World";
+		java += world;
+		System.out.println(java == javaworld);
+	}
+}
