0fae5c026d85abf72e5402608f2e9061941dab0c
Edgar M. Marquez Aylas <edgardma@gmail.com>
2019-10-23 22:51:44 -0500

    Test 11

---
+++ examenJavaSE11ProgrammerI/src/com/examen/java/se11/capitulo1/test11/Test.java
@@ -0,0 +1,21 @@
+/*
+package com.examen.java.se11.capitulo1.test11;
+
+class M {
+	public void main(String[] args) { // Line n1
+		System.out.println("M");
+	}
+}
+
+class N extends M {
+	public static void main(String[] args) {  // Line n2 -> Esta linea no compila por un error
+		new M().main(args); // Line n3
+	}
+}
+
+public class Test {
+	public static void main(String[] args) {
+		N.main(args); // Line 4
+	}
+}
+*/
