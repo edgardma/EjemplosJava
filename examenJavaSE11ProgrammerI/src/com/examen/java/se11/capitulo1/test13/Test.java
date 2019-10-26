b3b5b50614daf9aa17e22345e0833fd7d36773e1
Edgar M. Marquez Aylas <edgardma@gmail.com>
2019-10-25 23:41:25 -0500

    Test 13

---
+++ examenJavaSE11ProgrammerI/src/com/examen/java/se11/capitulo1/test13/Test.java
@@ -0,0 +1,8 @@
+package com.examen.java.se11.capitulo1.test13;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(53 / 2.0); // 26.5
+		System.out.println(53 % 2.0); // 1.0 -> Respuesta B: 
+	}
+}
