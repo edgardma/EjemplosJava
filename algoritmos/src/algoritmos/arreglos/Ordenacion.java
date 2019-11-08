a21947de79578a28d5e843919d5605f88dcb7f23
Edgar M. Marquez Aylas <edgardma@gmail.com>
2019-11-07 23:20:13 -0500

    Metodo Burbuja

--- algoritmos/src/algoritmos/arreglos/Ordenacion.java
+++ algoritmos/src/algoritmos/arreglos/Ordenacion.java
@@ -1,19 +1,30 @@
 package algoritmos.arreglos;
 
 public class Ordenacion {
+	// http://buenasintencions.blogspot.com/2011/11/metodo-de-la-burbuja-en-java.html
+	public static void main(String[] args) {
+		int arreglo[] = {8,6,7,2,1,8,6,8,7,1,9,7,7,10};
+		int arregloOrdenado[] = metodoLaBurbuja(arreglo);
+		
+		for(int i = 0; i < arregloOrdenado.length; i++) {
+			System.out.println(arregloOrdenado[i]);
+		}
+	}
+	
 	public static int[] metodoLaBurbuja(int[] arreglo) {
 		int auxiliar;
-		int[] arregloOrdenado = null;
+		int[] arregloOrdenado;
 		
 		for(int i = 2; i < arreglo.length; i++) {
-			for(int j = 0; j < arreglo.length; j++) {
+			for(int j = 0; j < arreglo.length - i; j++) {
 				if(arreglo[j] > arreglo[j + 1]) {
 					auxiliar = arreglo[j];
 					arreglo[j] = arreglo[j + 1];
-					arreglo[j+1] = auxiliar;
+					arreglo[j + 1] = auxiliar;
 				}
 			}
 		}
+		arregloOrdenado = arreglo;
 		
 		return arregloOrdenado;
 	}
