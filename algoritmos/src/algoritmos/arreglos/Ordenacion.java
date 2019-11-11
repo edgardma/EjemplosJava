eb5ef8f323c33b5a1cf229a7bbd48f592e1a6417
Edgar M. Marquez Aylas <edgardma@gmail.com>
2019-11-10 23:12:23 -0500

    Actualizar Ordenacion Burbuja

--- algoritmos/src/algoritmos/arreglos/Ordenacion.java
+++ algoritmos/src/algoritmos/arreglos/Ordenacion.java
@@ -1,5 +1,7 @@
 package algoritmos.arreglos;
 
+import java.util.Arrays;
+
 public class Ordenacion {
 	// http://buenasintencions.blogspot.com/2011/11/metodo-de-la-burbuja-en-java.html
 	public static void main(String[] args) {
@@ -9,13 +11,18 @@ public class Ordenacion {
 		for(int i = 0; i < arregloOrdenado.length; i++) {
 			System.out.println(arregloOrdenado[i]);
 		}
+		
+		Arrays.sort(arreglo);
+		for(int i = 0; i < arreglo.length; i++) {
+			System.out.println(arreglo[i]);
+		}
 	}
 	
 	public static int[] metodoLaBurbujaAsc(int[] arreglo) {
 		int auxiliar;
 		int[] arregloOrdenado;
 		
-		for(int i = 1; i < arreglo.length; i++) {
+		for(int i = 2; i < arreglo.length; i++) {
 			for(int j = 0; j < arreglo.length - i; j++) {
 				if(arreglo[j] > arreglo[j + 1]) {
 					auxiliar = arreglo[j];
