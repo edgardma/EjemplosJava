f38034a7002db71c09fd4d21aab56a51b2b0999a
Edgar M. Marquez Aylas <edgardma@gmail.com>
2019-11-21 22:42:12 -0500

    Crear clase Product

--- 1.Patrones/factoryMethod/src/pe/com/dyd/patrones/ejemplos/factoryMethod/entity/Product.java
+++ 1.Patrones/factoryMethod/src/pe/com/dyd/patrones/ejemplos/factoryMethod/entity/Product.java
@@ -1,5 +1,38 @@
 package pe.com.dyd.patrones.ejemplos.factoryMethod.entity;
 
 public class Product {
+	private Long idProduct;
+	private String productName;
+	private double price;
+	
+	public Product(Long idProduct, String productName, double price) {
+		super();
+		this.idProduct = idProduct;
+		this.productName = productName;
+		this.price = price;
+	}
 
+	public Long getIdProduct() {
+		return idProduct;
+	}
+
+	public void setIdProduct(Long idProduct) {
+		this.idProduct = idProduct;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 }
