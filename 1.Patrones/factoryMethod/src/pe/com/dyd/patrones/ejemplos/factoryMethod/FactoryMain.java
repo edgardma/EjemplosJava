887e7a1411bff3619e65e1ab75c42fb791a6e23f
Edgar M. Marquez Aylas <edgardma@gmail.com>
2019-11-22 22:27:15 -0500

    Terminar el ejemplo

--- 1.Patrones/factoryMethod/src/pe/com/dyd/patrones/ejemplos/factoryMethod/FactoryMain.java
+++ 1.Patrones/factoryMethod/src/pe/com/dyd/patrones/ejemplos/factoryMethod/FactoryMain.java
@@ -1,5 +1,25 @@
 package pe.com.dyd.patrones.ejemplos.factoryMethod;
 
-public class FactoryMain {
+import java.util.List;
 
+import pe.com.dyd.patrones.ejemplos.factoryMethod.dao.ProductDAO;
+import pe.com.dyd.patrones.ejemplos.factoryMethod.entity.Product;
+
+public class FactoryMain {
+	public static void main(String[] args) {
+		Product productA = new Product(1L, "Product A", 100);
+		Product productB = new Product(2L, "Product B", 100);
+		
+		ProductDAO productDAO = new ProductDAO();
+		
+		productDAO.saveProduct(productA);
+		productDAO.saveProduct(productB);
+		
+		List<Product> products = productDAO.findAllProducts();
+		System.out.print("Product size ==>" + products.size());
+		
+		for(Product product:products) {
+			System.out.println(product);
+		}
+	}
 }
--- 1.Patrones/factoryMethod/src/pe/com/dyd/patrones/ejemplos/factoryMethod/dao/ProductDAO.java
+++ 1.Patrones/factoryMethod/src/pe/com/dyd/patrones/ejemplos/factoryMethod/dao/ProductDAO.java
@@ -1,5 +1,69 @@
 package pe.com.dyd.patrones.ejemplos.factoryMethod.dao;
 
-public class ProductDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import pe.com.dyd.patrones.ejemplos.factoryMethod.DBFactory;
+import pe.com.dyd.patrones.ejemplos.factoryMethod.IDBAdapter;
+import pe.com.dyd.patrones.ejemplos.factoryMethod.entity.Product;
+
+public class ProductDAO {
+	private IDBAdapter dbAdapter;
+	
+	public ProductDAO() {
+		dbAdapter = DBFactory.getDefaultDBAdapter();
+	}
+	
+	public List<Product> findAllProducts() {
+		Connection connection = dbAdapter.getConnection();
+		List<Product> productList = new ArrayList<>();
+		
+		try {
+			PreparedStatement stament = connection.prepareStatement(
+					"SELECT idProductos, productName, productPrice FROM Productos");
+			ResultSet results = stament.executeQuery();
+			while (results.next()) {
+				productList.add(new Product(
+						results.getLong(1), 
+						results.getString(2),
+						results.getDouble(3)));
+			}
+			return productList;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			try {
+				connection.close();
+			} catch(Exception e) {
+				
+			}
+		}
+	}
+	
+	public boolean saveProduct(Product product) {
+		Connection connection = dbAdapter.getConnection();
+		
+		try {
+			PreparedStatement stament = connection.prepareStatement(
+					"INSERT INTO Productos(idProductos, productName, productPrice) VALUES (?, ?, ?)");
+			stament.setLong(1, product.getIdProduct());
+			stament.setString(2, product.getProductName());
+			stament.setDouble(3, product.getPrice());
+			
+			return true;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			try {
+				connection.close();
+			} catch(Exception e) {
+				
+			}
+		}
+	}
 }
