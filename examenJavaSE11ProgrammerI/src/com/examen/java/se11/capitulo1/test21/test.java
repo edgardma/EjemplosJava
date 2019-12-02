f61f5cc15f0b24cd8851f41558639e5d56b81cc0
Edgar M. Marquez Aylas <edgardma@gmail.com>
2019-12-01 21:38:42 -0500

    Test 21

---
+++ examenJavaSE11ProgrammerI/src/com/examen/java/se11/capitulo1/test21/test.java
@@ -0,0 +1,10 @@
+package com.examen.java.se11.capitulo1.test21;
+
+public class test {
+	public static void main(String[] args) {
+		do {
+			System.out.print("SLOW-");
+		} while(false);
+		System.out.println("DOWN");
+	}
+}
