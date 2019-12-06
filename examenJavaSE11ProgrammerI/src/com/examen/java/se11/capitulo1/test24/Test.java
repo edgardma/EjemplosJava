d5c4f7b7d02d133646d9c5f1620a21ded62c8361
Edgar M. Marquez Aylas <edgardma@gmail.com>
2019-12-05 22:53:42 -0500

    Test 24

---
+++ examenJavaSE11ProgrammerI/src/com/examen/java/se11/capitulo1/test24/Test.java
@@ -0,0 +1,11 @@
+package com.examen.java.se11.capitulo1.test24;
+
+public class Test {
+	public static void main(String[] args) {
+		byte b1 = 10; // Line 1
+		int i1 = b1; // Line 2
+		// byte b2 = i1; // Line 3 // Causes compilation error
+		
+		//System.out.println(b1 + i1 + b2);		
+	}
+}
