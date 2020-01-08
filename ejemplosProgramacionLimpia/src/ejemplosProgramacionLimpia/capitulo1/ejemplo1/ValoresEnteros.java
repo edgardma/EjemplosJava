f6a1c4fb848d4160137d09134a91eebf3e64b9e3
Edgar M. Marquez Aylas <edgardma@gmail.com>
2020-01-07 22:46:23 -0500

    Ejemplo 1

---
+++ ejemplosProgramacionLimpia/src/ejemplosProgramacionLimpia/capitulo1/ejemplo1/ValoresEnteros.java
@@ -0,0 +1,23 @@
+package ejemplosProgramacionLimpia.capitulo1.ejemplo1;
+
+public class ValoresEnteros {
+	public static void main(String[] args) {
+		byte dias;
+		short contador;
+		
+		short resultado = 42;
+		
+		short codiog, edad, media;
+		
+		int i, j, k;
+		
+		i = j = k = 0;
+		
+		long sueldoFutbolista = 23489343;
+		long valorGoogle = 666000666;
+		
+		System.out.println("El sentidad de la vida: " + resultado);
+		
+		System.out.println("El sueldo de un futbolista es: " + sueldoFutbolista);
+	}
+}
---
+++ ejemplosProgramacionLimpia/src/ejemplosProgramacionLimpia/capitulo1/ejemplo2/ValoresReales.java
@@ -0,0 +1,7 @@
+package ejemplosProgramacionLimpia.capitulo1.ejemplo2;
+
+public class ValoresReales {
+	public static void main(String[] args) {
+		
+	}
+}
