64ff4e929e26afc5171209aed8883f85d671867c
Edgar M. Marquez Aylas <edgardma@gmail.com>
2020-01-08 22:24:48 -0500

    Ejemplo 2

--- ejemplosProgramacionLimpia/src/ejemplosProgramacionLimpia/capitulo1/ejemplo2/ValoresReales.java
+++ ejemplosProgramacionLimpia/src/ejemplosProgramacionLimpia/capitulo1/ejemplo2/ValoresReales.java
@@ -2,6 +2,12 @@ package ejemplosProgramacionLimpia.capitulo1.ejemplo2;
 
 public class ValoresReales {
 	public static void main(String[] args) {
+		float temperatura;
+		float peso = 78.9F;
+		double saldoCuentaCorriente = 3423343.43D;
+		double masaJupiter = 4.6E+9D;
 		
+		System.out.println("Tu peso es: " + peso + ", y tu saldo: " + saldoCuentaCorriente);
+		System.out.println("La masa Jupiter: " + masaJupiter);
 	}
 }
