216367ae5ec6a2e613ce8ce3325938ae3307c71e
Edgar M. Marquez Aylas <edgardma@gmail.com>
2020-02-28 23:13:34 -0500

    Patron Singleton - Implementacion 1

--- ejemplosPatrones/src/pe/com/dyd/patrones/ejemplos/factoryMethod/util/PropertiesUtil.java
+++ ejemplosPatrones/src/pe/com/dyd/patrones/ejemplos/factoryMethod/util/PropertiesUtil.java
@@ -1,5 +1,12 @@
 package pe.com.dyd.patrones.ejemplos.factoryMethod.util;
 
+import java.util.Properties;
+
 public class PropertiesUtil {
+
+	public static Properties loadProperty(String configurationProp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- ejemplosPatrones/src/pe/com/dyd/patrones/ejemplos/singleton/configuracion/ConfigurationSingleton.java
+++ ejemplosPatrones/src/pe/com/dyd/patrones/ejemplos/singleton/configuracion/ConfigurationSingleton.java
@@ -1,5 +1,32 @@
 package pe.com.dyd.patrones.ejemplos.singleton.configuracion;
 
-public class ConfigurationSingleton {
+import java.util.Properties;
 
+import pe.com.dyd.patrones.ejemplos.factoryMethod.util.PropertiesUtil;
+
+public class ConfigurationSingleton {
+	private static ConfigurationSingleton singleton;
+	private static final String CONFIGURATION_PROP = "META-INF/Configuration.properties";
+	private static final String APP_NAME_PROP = "appName";
+	private static final String APP_VERSION_PROP = "appVersion";
+	
+	private String appName;
+	private String appVersion;
+	
+	private ConfigurationSingleton() {
+		Properties prop = PropertiesUtil.loadProperty(CONFIGURATION_PROP);
+		this.appName = prop.getProperty(APP_NAME_PROP);
+		this.appVersion = prop.getProperty(APP_VERSION_PROP);
+	}
+	
+	private static synchronized void createInstance() {
+		if (singleton == null) {
+			singleton = new ConfigurationSingleton();
+		}
+	}
+	
+	public static ConfigurationSingleton getInstance() {
+		createInstance();
+		return singleton;
+	}
 }
