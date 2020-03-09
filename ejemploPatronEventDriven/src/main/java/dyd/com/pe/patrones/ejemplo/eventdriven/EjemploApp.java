0a09a148c00d2bf3770bb0289730e27d9eea6c55
Edgar M. Marquez Aylas <edgardma@gmail.com>
2020-03-08 22:20:13 -0500

    Patron EventDriven

---
+++ ejemploPatronEventDriven/src/main/java/dyd/com/pe/patrones/ejemplo/eventdriven/EjemploApp.java
@@ -0,0 +1,13 @@
+package dyd.com.pe.patrones.ejemplo.eventdriven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploApp.class, args);
+	}
+
+}
---
+++ ejemploPatronEventDriven/src/test/java/dyd/com/pe/patrones/ejemploPatronEventDriven/EjemploPatronEventDrivenApplicationTests.java
@@ -0,0 +1,13 @@
+package dyd.com.pe.patrones.ejemploPatronEventDriven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploPatronEventDrivenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
