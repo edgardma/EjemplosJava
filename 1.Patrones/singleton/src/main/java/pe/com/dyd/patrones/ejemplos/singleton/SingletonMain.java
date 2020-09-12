bc54cdd3313dcbb33fa26cb0d57af6cc58809bbf
Edgard M. Marquez Aylas <edgardma@gmail.com>
2020-09-11 19:17:44 -0500

    Implementar el Patron Singleton

--- 1.Patrones/singleton/src/main/java/pe/com/dyd/patrones/ejemplos/singleton/SingletonMain.java
+++ 1.Patrones/singleton/src/main/java/pe/com/dyd/patrones/ejemplos/singleton/SingletonMain.java
@@ -1,5 +1,24 @@
 package pe.com.dyd.patrones.ejemplos.singleton;
 
 public class SingletonMain {
-
+	public static void main(String[] args) {
+		ConfigurationSingleton singletonA = ConfigurationSingleton.getInstance();
+		ConfigurationSingleton singletonB = ConfigurationSingleton.getInstance();
+		
+		System.out.println(singletonA);
+		System.out.println(singletonB);
+		System.out.println("Misma referencia ==> " + (singletonA == singletonB));
+		
+		singletonA.setAppName("Singleton Pattern");
+		singletonB.setAppVersion("1.0.X");
+		
+		System.out.println("Singleton A ==> " + singletonA);
+		System.out.println("Singleton B ==> " + singletonB);
+		
+		singletonA = null;
+		singletonB = null;
+		
+		singletonA = ConfigurationSingleton.getInstance();
+		System.out.println("Singleton A ==> " + singletonA);
+	}
 }
--- 1.Patrones/singleton/src/main/java/pe/com/dyd/patrones/ejemplos/singleton/util/PropertiesUtil.java
+++ 1.Patrones/singleton/src/main/java/pe/com/dyd/patrones/ejemplos/singleton/util/PropertiesUtil.java
@@ -1,5 +1,25 @@
 package pe.com.dyd.patrones.ejemplos.singleton.util;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class PropertiesUtil {
+
+	public static Properties loadProperty(String propertiesURL) {
+		try {
+			System.out.println(propertiesURL);
+			Properties properties = new Properties();
+			InputStream inputStream = PropertiesUtil.class.getClassLoader().getResourceAsStream(propertiesURL);
+			properties.load(inputStream);
+			return properties;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private PropertiesUtil() {
+		
+	}
 
 }
