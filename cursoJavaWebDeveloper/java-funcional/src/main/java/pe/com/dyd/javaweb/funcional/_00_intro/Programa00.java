c7fc60b1189bfb9a7a6e5c538a73b947387a12fe
Edgard M. Marquez Aylas <edgardma@gmail.com>
2020-10-03 16:44:53 -0500

    Ejemplo Java Funcional

---
+++ cursoJavaWebDeveloper/java-funcional/src/main/java/pe/com/dyd/javaweb/funcional/_00_intro/Programa00.java
@@ -0,0 +1,23 @@
+package pe.com.dyd.javaweb.funcional._00_intro;
+
+public class Programa00 {
+	
+	private Integer sumatoria = 0;
+	
+	public static void main(String[] args) {
+		Programa00 programa = new Programa00();
+		System.out.println(programa.suma2(3, 5));
+		System.out.println(programa.suma2(3, 5));
+	}
+	
+	// Este es una funcion pura, es decir, recibiendo los mismos valores de parametros
+	// se obtendrá el mismo resultado, "n" veces
+	public int suma(int a, int b) {
+		return a + b;
+	}
+	
+	public int suma2(int a, int b) {
+		sumatoria = sumatoria + suma(a, b);
+		return sumatoria;
+	}
+}
