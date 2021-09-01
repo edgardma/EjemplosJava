3130a5f7d4f782edbdd9d4c186bdf1f28e94d539
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-09-01 12:17:33 -0500

    Ejemplo de Spring con Flux

---
+++ 2.Spring/spring-boot-reactor/src/main/java/pe/com/dyd/ejemplos/spring/reactor/app/SpringBootReactorApplication.java
@@ -0,0 +1,29 @@
+package pe.com.dyd.ejemplos.spring.reactor.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootReactorApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Imprimir en la consola
+		Flux<String> nombres = Flux.just("Andres", "Pedro", "Diego", "Juan").
+				doOnNext(System.out::println);
+		// Imprime en el log
+		nombres.subscribe(e -> log.info(e));
+	}
+
+}
---
+++ 2.Spring/spring-boot-reactor/src/test/java/pe/com/dyd/ejemplos/spring/reactor/app/SpringBootReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.dyd.ejemplos.spring.reactor.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
