2be474b6d95d9477b3eab1d69d43c8ac2992c025
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-09-03 23:08:00 -0500

    Programacion Reactiva - Flux desde List

--- 2.Spring/spring-boot-reactor/src/main/java/pe/com/dyd/ejemplos/spring/reactor/app/SpringBootReactorApplication.java
+++ 2.Spring/spring-boot-reactor/src/main/java/pe/com/dyd/ejemplos/spring/reactor/app/SpringBootReactorApplication.java
@@ -20,6 +20,49 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		prueba3();
+	}
+	
+	private void prueba4() {
+		List<String> usuariosList = new ArrayList<>();
+		usuariosList.add("Andres Guzman");
+		usuariosList.add("Pedro Fulano");
+		usuariosList.add("Maria Fulana");
+		usuariosList.add("Diego Sultano");
+		usuariosList.add("Juan Mengano");
+		usuariosList.add("Bruce Lee");
+		usuariosList.add("Bruce Willis");
+		
+		Flux<String> nombres = Flux.fromIterable(usuariosList);
+		Flux<Usuario> usuarios = nombres.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+				.filter(usuario -> {
+					return usuario.getNombres().toLowerCase().equals("bruce");
+				})
+				.doOnNext(usuario -> {
+					if (usuario == null) {
+						throw new RuntimeException("Nombres no pueden ser vacíos");
+					}
+					System.out.println(usuario.getNombres().concat(" ").concat(usuario.getApellidos()));
+				})
+				.map(usuario -> {
+					String nombre = usuario.getNombres().toLowerCase();
+					usuario.setNombres(nombre);
+					return usuario;
+				});
+		
+		// Imprime en el log
+		usuarios.subscribe(
+				e -> log.info(e.toString()),
+				error -> log.error(error.getMessage()),
+				new Runnable() {
+					@Override
+					public void run() {
+						log.info("Ha finalizado la ejecución del observable con éxito!");
+					}
+				});
+	}
+	
+	private void prueba3() {
 		// Imprimir en la consola
 		// Flux<String> nombres = Flux.just("Andres", "Pedro", "", "Diego", "Juan") // Con error
 		Flux<Usuario> nombres = Flux.just("Andres Guzman", "Pedro Fulano", "Maria Fulana", "Diego Sultano", "Juan Mengano", "Bruce Lee", "Bruce Willis")
