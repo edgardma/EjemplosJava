70d0329c827e1a3a8441ca5efa3f7e40081a405a
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-09-03 23:25:57 -0500

    Programacion Reactiva - Ejemplo con flatMap

--- 2.Spring/spring-boot-reactor/src/main/java/pe/com/dyd/ejemplos/spring/reactor/app/SpringBootReactorApplication.java
+++ 2.Spring/spring-boot-reactor/src/main/java/pe/com/dyd/ejemplos/spring/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,8 @@
 package pe.com.dyd.ejemplos.spring.reactor.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import pe.com.dyd.ejemplos.spring.reactor.app.models.Usuario;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
@@ -20,7 +24,35 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		prueba3();
+		ejemploFlatMap();
+	}
+	
+	private void ejemploFlatMap() {
+		List<String> usuariosList = new ArrayList<>();
+		usuariosList.add("Andres Guzman");
+		usuariosList.add("Pedro Fulano");
+		usuariosList.add("Maria Fulana");
+		usuariosList.add("Diego Sultano");
+		usuariosList.add("Juan Mengano");
+		usuariosList.add("Bruce Lee");
+		usuariosList.add("Bruce Willis");
+		
+		Flux.fromIterable(usuariosList)
+				.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+				.flatMap(usuario -> {
+					if(usuario.getNombres().equalsIgnoreCase("bruce")) {
+						return Mono.just(usuario);
+					} else {
+						return Mono.empty();
+					}
+				})
+				.map(usuario -> {
+					String nombre = usuario.getNombres().toLowerCase();
+					usuario.setNombres(nombre);
+					return usuario;
+				})
+				.subscribe(
+				e -> log.info(e.toString()));
 	}
 	
 	private void prueba4() {
@@ -64,7 +96,68 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 	
 	private void prueba3() {
 		// Imprimir en la consola
-		// Flux<String> nombres = Flux.just("Andres", "Pedro", "", "Diego", "Juan") // Con error
+		Flux<String> nombres = Flux.just("Andres Guzman", "Pedro Fulano", "Maria Fulana", "Diego Sultano", "Juan Mengano", "Bruce Lee", "Bruce Willis");
+		Flux<Usuario> usuarios = nombres.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+				.filter(usuario -> {
+					return usuario.getNombres().toLowerCase().equals("bruce");
+				})
+				.doOnNext(usuario -> {
+					if (usuario == null) {
+						throw new RuntimeException("Nombres no pueden ser vacíos");
+					}
+					System.out.println(usuario.getNombres().concat(" ").concat(usuario.getApellidos()));
+				})
+				.map(usuario -> {
+					String nombre = usuario.getNombres().toLowerCase();
+					usuario.setNombres(nombre);
+					return usuario;
+				});
+		
+		// Imprime en el log
+		usuarios.subscribe(
+				e -> log.info(e.toString()),
+				error -> log.error(error.getMessage()),
+				new Runnable() {
+					@Override
+					public void run() {
+						log.info("Ha finalizado la ejecución del observable con éxito!");
+					}
+				});
+	}
+	
+	private void prueba2() {
+		// Imprimir en la consola
+		Flux<String> nombres = Flux.just("Andres Guzman", "Pedro Fulano", "Maria Fulana", "Diego Sultano", "Juan Mengano", "Bruce Lee", "Bruce Willis");
+		nombres.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+				.filter(usuario -> {
+					return usuario.getNombres().toLowerCase().equals("bruce");
+				})
+				.doOnNext(usuario -> {
+					if (usuario == null) {
+						throw new RuntimeException("Nombres no pueden ser vacíos");
+					}
+					System.out.println(usuario.getNombres().concat(" ").concat(usuario.getApellidos()));
+				})
+				.map(usuario -> {
+					String nombre = usuario.getNombres().toLowerCase();
+					usuario.setNombres(nombre);
+					return usuario;
+				});
+		
+		// Imprime en el log
+		nombres.subscribe(
+				e -> log.info(e.toString()),
+				error -> log.error(error.getMessage()),
+				new Runnable() {
+					@Override
+					public void run() {
+						log.info("Ha finalizado la ejecución del observable con éxito!");
+					}
+				});
+	}
+	
+	private void prueba1() {
+		// Imprimir en la consola
 		Flux<Usuario> nombres = Flux.just("Andres Guzman", "Pedro Fulano", "Maria Fulana", "Diego Sultano", "Juan Mengano", "Bruce Lee", "Bruce Willis")
 				.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
 				.filter(usuario -> {
@@ -74,7 +167,7 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 					if (usuario == null) {
 						throw new RuntimeException("Nombres no pueden ser vacíos");
 					}
-					System.out.println(usuario.getNombres());
+					System.out.println(usuario.getNombres().concat(" ").concat(usuario.getApellidos()));
 				})
 				.map(usuario -> {
 					String nombre = usuario.getNombres().toLowerCase();
