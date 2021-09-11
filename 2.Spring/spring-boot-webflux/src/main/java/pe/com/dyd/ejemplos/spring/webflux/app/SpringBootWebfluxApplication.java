41dcfc6fbb407bf27340117b82c0380a9ba347a4
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-09-10 22:49:43 -0500

    Ejemplo Spring Boot WebFlux - Eliminar los datos al iniciar la app

--- 2.Spring/spring-boot-webflux/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/SpringBootWebfluxApplication.java
+++ 2.Spring/spring-boot-webflux/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/SpringBootWebfluxApplication.java
@@ -1,10 +1,13 @@
 package pe.com.dyd.ejemplos.spring.webflux.app;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 import pe.com.dyd.ejemplos.spring.webflux.app.models.dao.ProductoDao;
 import pe.com.dyd.ejemplos.spring.webflux.app.models.documents.Producto;
@@ -15,6 +18,9 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProductoDao dao;
+	@Autowired
+	private ReactiveMongoTemplate mongoTemplate;
+	
 	private static final Logger log = org.slf4j.LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
 	
 	public static void main(String[] args) {
@@ -23,6 +29,8 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		mongoTemplate.dropCollection("productos")
+			.subscribe();
 		
 		Flux.just(
 				new Producto("TV Panasonic", 451.16), 
@@ -30,10 +38,15 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 				new Producto("Laptop HP", 1485.99), 
 				new Producto("Impresora Epson", 156.23), 
 				new Producto("iPod Apple", 550.13), 
+				new Producto("Labadora Samsung", 985.99),
+				new Producto("Refrigerador LG", 1550.69),
 				new Producto("Cocina Mabe", 100.02)
 				)
-		.flatMap(producto -> dao.save(producto))
-		.subscribe(producto -> log.info("Insert: " + producto.getId() + "-" + producto.getNombre()));
+			.flatMap(producto -> {
+				producto.setCreatAt(new Date());
+				return dao.save(producto);
+				})
+			.subscribe(producto -> log.info("Insert: " + producto.getId() + "-" + producto.getNombre()));
 	}
 
 }
