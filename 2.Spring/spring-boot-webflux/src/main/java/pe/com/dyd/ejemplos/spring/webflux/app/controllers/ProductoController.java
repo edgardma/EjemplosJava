532ae4ccc9dcd1079253351b06d2230f873a7ac3
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-09-10 23:27:59 -0500

    Ejemplo Spring Boot WebFlux - Reactive Data Driven manejar contrapresion

--- 2.Spring/spring-boot-webflux/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/controllers/ProductoController.java
+++ 2.Spring/spring-boot-webflux/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/controllers/ProductoController.java
@@ -1,11 +1,14 @@
 package pe.com.dyd.ejemplos.spring.webflux.app.controllers;
 
+import java.time.Duration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
 import pe.com.dyd.ejemplos.spring.webflux.app.models.dao.ProductoDao;
 import pe.com.dyd.ejemplos.spring.webflux.app.models.documents.Producto;
@@ -30,6 +33,23 @@ public class ProductoController {
 		productos.subscribe(prod -> log.info(prod.getNombre()));
 		
 		model.addAttribute("productos", productos);
+		model.addAttribute("titulo", "Listado de productos");
+		
+		return "listar";
+	}
+	
+	@GetMapping("/listar-datadriver")
+	public String listarDataDriver(Model model) {
+		Flux<Producto> productos = dao.findAll()
+			.map(producto -> {
+				producto.setNombre(producto.getNombre().toUpperCase());
+				return producto;
+			})
+			.delayElements(Duration.ofSeconds(1));
+		
+		productos.subscribe(prod -> log.info(prod.getNombre()));
+		
+		model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos, 1));
 		model.addAttribute("titulo", "Listado de productos");
 		
 		return "listar";
