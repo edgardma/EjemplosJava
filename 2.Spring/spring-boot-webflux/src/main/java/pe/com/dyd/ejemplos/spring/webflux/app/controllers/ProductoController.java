7342140ba52e2e5d4b0181d535efab7d3ab3c0d0
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-09-12 19:17:24 -0500

    Ejemplo Spring Boot WebFlux - Modo Chunked para contrapresion

--- 2.Spring/spring-boot-webflux/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/controllers/ProductoController.java
+++ 2.Spring/spring-boot-webflux/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/controllers/ProductoController.java
@@ -54,4 +54,19 @@ public class ProductoController {
 		
 		return "listar";
 	}
+	
+	@GetMapping({"/listar-full"})
+	public String listarFull(Model model) {
+		Flux<Producto> productos = dao.findAll()
+			.map(producto -> {
+				producto.setNombre(producto.getNombre().toUpperCase());
+				return producto;
+			})
+			.repeat(5000);
+		
+		model.addAttribute("productos", productos);
+		model.addAttribute("titulo", "Listado de productos");
+		
+		return "listar";
+	}
 }
