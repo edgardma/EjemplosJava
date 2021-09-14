778fef57a0894e2326cce987853404e9a494c247
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-09-13 21:46:59 -0500

    Crear Componente Service de Productos

--- 2.Spring/spring-boot-webflux/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/controllers/ProductoController.java
+++ 2.Spring/spring-boot-webflux/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/controllers/ProductoController.java
@@ -10,25 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
-import pe.com.dyd.ejemplos.spring.webflux.app.models.dao.ProductoDao;
 import pe.com.dyd.ejemplos.spring.webflux.app.models.documents.Producto;
+import pe.com.dyd.ejemplos.spring.webflux.app.models.services.ProductoService;
 import reactor.core.publisher.Flux;
 
 @Controller
 public class ProductoController {
 	
 	@Autowired
-	private ProductoDao dao;
+	private ProductoService service;
 	
 	private static final Logger log = LoggerFactory.getLogger(ProductoController.class);
 	
 	@GetMapping({"/listar", "/"})
 	public String listar(Model model) {
-		Flux<Producto> productos = dao.findAll()
-			.map(producto -> {
-				producto.setNombre(producto.getNombre().toUpperCase());
-				return producto;
-			});
+		Flux<Producto> productos = service.findAllConNombreUpperCase();
 		
 		productos.subscribe(prod -> log.info(prod.getNombre()));
 		
@@ -40,11 +36,7 @@ public class ProductoController {
 	
 	@GetMapping("/listar-datadriver")
 	public String listarDataDriver(Model model) {
-		Flux<Producto> productos = dao.findAll()
-			.map(producto -> {
-				producto.setNombre(producto.getNombre().toUpperCase());
-				return producto;
-			})
+		Flux<Producto> productos = service.findAllConNombreUpperCase()
 			.delayElements(Duration.ofSeconds(1));
 		
 		productos.subscribe(prod -> log.info(prod.getNombre()));
@@ -57,12 +49,7 @@ public class ProductoController {
 	
 	@GetMapping({"/listar-full"})
 	public String listarFull(Model model) {
-		Flux<Producto> productos = dao.findAll()
-			.map(producto -> {
-				producto.setNombre(producto.getNombre().toUpperCase());
-				return producto;
-			})
-			.repeat(5000);
+		Flux<Producto> productos = service.findAllConNombreUpperCaseRepeat();
 		
 		model.addAttribute("productos", productos);
 		model.addAttribute("titulo", "Listado de productos");
@@ -72,12 +59,7 @@ public class ProductoController {
 	
 	@GetMapping("/listar-chunked")
 	public String listarChunked(Model model) {
-		Flux<Producto> productos = dao.findAll()
-			.map(producto -> {
-				producto.setNombre(producto.getNombre().toUpperCase());
-				return producto;
-			})
-			.repeat(5000);
+		Flux<Producto> productos = service.findAllConNombreUpperCaseRepeat();
 		
 		model.addAttribute("productos", productos);
 		model.addAttribute("titulo", "Listado de productos");
---
+++ 2.Spring/spring-boot-webflux/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/models/services/ProductoService.java
@@ -0,0 +1,21 @@
+package pe.com.dyd.ejemplos.spring.webflux.app.models.services;
+
+import pe.com.dyd.ejemplos.spring.webflux.app.models.documents.Producto;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface ProductoService {
+	
+	public Flux<Producto> findAll();
+	
+	public Flux<Producto> findAllConNombreUpperCase();
+	
+	public Flux<Producto> findAllConNombreUpperCaseRepeat();
+	
+	public Mono<Producto> findById(String id);
+	
+	public Mono<Producto> save(Producto producto);
+	
+	public Mono<Void> delete(Producto producto);
+	
+}
