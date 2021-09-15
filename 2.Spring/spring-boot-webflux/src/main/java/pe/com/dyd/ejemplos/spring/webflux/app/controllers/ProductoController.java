f910da2d1e9ea4bfc8cf0523aabbb9e45f39b383
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-09-15 13:10:53 -0500

    Programacion Reactiva CRUD - Metodo eliminar en Controlador y vista

--- 2.Spring/spring-boot-webflux/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/controllers/ProductoController.java
+++ 2.Spring/spring-boot-webflux/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/controllers/ProductoController.java
@@ -109,6 +109,25 @@ public class ProductoController {
 		}
 	}
 	
+	@GetMapping("/eliminar/{id}")
+	public Mono<String> eliminar(@PathVariable String id) {
+		return service.findById(id)
+				.defaultIfEmpty(new Producto())
+				.flatMap(p -> {
+					if (p.getId() == null) {
+						return Mono.error(new InterruptedException("No existe el producto a eliminar!!"));
+					}
+					return Mono.just(p);
+				})
+				.flatMap(p -> {
+					log.info("Eliminando producto: " + p.getNombre());
+					log.info("Eliminando producto Id: " + p.getId());
+					return service.delete(p);
+				})
+				.then(Mono.just("redirect:/listar?success=producto+eliminado+con+exito"))
+				.onErrorResume(ex -> Mono.just("redirect:/listar?error=no+existe+el+producto+a+eliminar"));
+	}
+	
 	@GetMapping("/listar-datadriver")
 	public String listarDataDriver(Model model) {
 		Flux<Producto> productos = service.findAllConNombreUpperCase()
