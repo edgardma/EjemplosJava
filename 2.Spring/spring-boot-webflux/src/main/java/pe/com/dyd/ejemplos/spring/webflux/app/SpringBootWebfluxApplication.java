a82b58ef041b95de28e1d57ed9da391959a5f202
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-09-15 15:52:30 -0500

    Programacion Reactiva CRUD - Categorias ejemplo y operadpr thenMany

--- 2.Spring/spring-boot-webflux/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/SpringBootWebfluxApplication.java
+++ 2.Spring/spring-boot-webflux/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/SpringBootWebfluxApplication.java
@@ -9,15 +9,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
-import pe.com.dyd.ejemplos.spring.webflux.app.models.dao.ProductoDao;
+import pe.com.dyd.ejemplos.spring.webflux.app.models.documents.Categoria;
 import pe.com.dyd.ejemplos.spring.webflux.app.models.documents.Producto;
+import pe.com.dyd.ejemplos.spring.webflux.app.models.services.ProductoService;
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SpringBootWebfluxApplication implements CommandLineRunner {
 	
 	@Autowired
-	private ProductoDao dao;
+	private ProductoService service;
 	@Autowired
 	private ReactiveMongoTemplate mongoTemplate;
 	
@@ -29,23 +30,39 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		mongoTemplate.dropCollection("productos")
-			.subscribe();
+		mongoTemplate.dropCollection("productos").subscribe();
+		mongoTemplate.dropCollection("categorias").subscribe();
 		
-		Flux.just(
-				new Producto("TV Panasonic", 451.16), 
-				new Producto("Camara Sony", 485.23), 
-				new Producto("Laptop HP", 1485.99), 
-				new Producto("Impresora Epson", 156.23), 
-				new Producto("iPod Apple", 550.13), 
-				new Producto("Labadora Samsung", 985.99),
-				new Producto("Refrigerador LG", 1550.69),
-				new Producto("Cocina Mabe", 100.02)
-				)
-			.flatMap(producto -> {
-				producto.setCreatAt(new Date());
-				return dao.save(producto);
-				})
+		Categoria electronico = new Categoria("Electrónico");
+		Categoria deporte = new Categoria("Deporte");
+		Categoria computacion = new Categoria("Computación");
+		Categoria muebles = new Categoria("Muebles");
+		
+		Flux.just(electronico, deporte, computacion, muebles)
+			.flatMap(service::saveCategoria)
+			.doOnNext(categoria -> {
+				log.info("Categoria creada: " + categoria.getNombre() + ", Id: " + categoria.getId());
+			})
+			.thenMany(
+					Flux.just(
+							new Producto("TV Panasonic", 451.16, electronico), 
+							new Producto("Camara Sony", 485.23, electronico), 
+							new Producto("Laptop HP", 1485.99, computacion), 
+							new Producto("Impresora Epson", 156.23, computacion), 
+							new Producto("iPod Apple", 550.13, electronico), 
+							new Producto("Labadora Samsung", 985.99, electronico),
+							new Producto("Refrigerador LG", 1550.69, electronico),
+							new Producto("Cocina Mabe", 100.02, electronico),
+							new Producto("Bianchi Bicicleta", 100.02, deporte),
+							new Producto("Zapatilla Nike", 100.02, deporte),
+							new Producto("Cama 2 plazas", 100.02, muebles)
+							
+							)
+						.flatMap(producto -> {
+							producto.setCreatAt(new Date());
+							return service.save(producto);
+							})
+					)
 			.subscribe(producto -> log.info("Insert: " + producto.getId() + "-" + producto.getNombre()));
 	}
 
