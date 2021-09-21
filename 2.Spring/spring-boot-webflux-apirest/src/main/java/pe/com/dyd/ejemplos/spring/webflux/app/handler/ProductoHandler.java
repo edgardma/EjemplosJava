c76a3b8c3d984ef4ff9c391b9806e7b92a613382
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-09-21 15:14:46 -0500

    Programacion Reactiva API REST - Handler GET ver detalle por ID

--- 2.Spring/spring-boot-webflux-apirest/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/handler/ProductoHandler.java
+++ 2.Spring/spring-boot-webflux-apirest/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/handler/ProductoHandler.java
@@ -3,6 +3,7 @@ package pe.com.dyd.ejemplos.spring.webflux.app.handler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import static org.springframework.web.reactive.function.BodyInserters.*;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -22,4 +23,14 @@ public class ProductoHandler {
 				.body(service.findAll(), Producto.class);
 	}
 	
+	public Mono<ServerResponse> ver(ServerRequest request) {
+		String id = request.pathVariable("id");
+		
+		return service.findById(id)
+				.flatMap(p -> ServerResponse
+						.ok()
+						.contentType(MediaType.APPLICATION_JSON_UTF8)
+						.body(fromObject(p)))
+						.switchIfEmpty(ServerResponse.notFound().build());
+	}
 }
