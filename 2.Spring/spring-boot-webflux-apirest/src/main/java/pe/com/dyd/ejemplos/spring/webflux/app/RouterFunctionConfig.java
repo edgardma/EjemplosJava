332e4c8a00c0225fb8c497f037247da5c0676065
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-09-21 00:22:40 -0500

    Programacion Reactiva API REST - Conf. comp. Router Function y handler

---
+++ 2.Spring/spring-boot-webflux-apirest/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/RouterFunctionConfig.java
@@ -0,0 +1,21 @@
+package pe.com.dyd.ejemplos.spring.webflux.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import pe.com.dyd.ejemplos.spring.webflux.app.handler.ProductoHandler;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
+@Configuration
+public class RouterFunctionConfig {
+	
+	@Bean
+	public RouterFunction<ServerResponse> routes(ProductoHandler handler) {
+		return route(GET("/api/v2/productos").or(GET("/api/v3/productos")), handler::listar);
+	}
+	
+}
---
+++ 2.Spring/spring-boot-webflux-apirest/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/handler/ProductoHandler.java
@@ -0,0 +1,25 @@
+package pe.com.dyd.ejemplos.spring.webflux.app.handler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import pe.com.dyd.ejemplos.spring.webflux.app.models.documents.Producto;
+import pe.com.dyd.ejemplos.spring.webflux.app.models.services.ProductoService;
+import reactor.core.publisher.Mono;
+
+@Component
+public class ProductoHandler {
+	
+	@Autowired
+	private ProductoService service;
+	
+	public Mono<ServerResponse> listar(ServerRequest request) {
+		return ServerResponse.ok()
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.body(service.findAll(), Producto.class);
+	}
+	
+}
