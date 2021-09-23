f2b381c7c3875edcedd5c4e658c115ab723752a1
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-09-22 19:39:56 -0500

    Programacion Reactiva API REST - Handler POST para crear

--- 2.Spring/spring-boot-webflux-apirest/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/handler/ProductoHandler.java
+++ 2.Spring/spring-boot-webflux-apirest/src/main/java/pe/com/dyd/ejemplos/spring/webflux/app/handler/ProductoHandler.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import static org.springframework.web.reactive.function.BodyInserters.*;
+
+import java.net.URI;
+import java.util.Date;
+
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -32,5 +36,21 @@ public class ProductoHandler {
 						.contentType(MediaType.APPLICATION_JSON_UTF8)
 						.body(fromObject(p)))
 						.switchIfEmpty(ServerResponse.notFound().build());
+	}
+	
+	public Mono<ServerResponse> crear(ServerRequest request) {
+		Mono<Producto> producto = request.bodyToMono(Producto.class);
+		
+		return producto.flatMap(p -> {
+			if(p.getCreatAt() == null) {
+				p.setCreatAt(new Date());
+			}
+			
+			return service.save(p);
+		})
+		.flatMap(p -> ServerResponse
+				.created(URI.create("/api/v2/productos/".concat(p.getId())))
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.body(fromObject(p)));
 	}
 }
