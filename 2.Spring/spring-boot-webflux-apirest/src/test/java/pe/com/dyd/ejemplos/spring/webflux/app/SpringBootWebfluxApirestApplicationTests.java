04dc17a403193a725893df253b7c6e56ac96c356
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-09-24 18:01:20 -0500

    Programacion Reactiva API REST - Test endpoint listar

--- 2.Spring/spring-boot-webflux-apirest/src/test/java/pe/com/dyd/ejemplos/spring/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ 2.Spring/spring-boot-webflux-apirest/src/test/java/pe/com/dyd/ejemplos/spring/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,13 +1,29 @@
 package pe.com.dyd.ejemplos.spring.webflux.app;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-class SpringBootWebfluxApirestApplicationTests {
+import pe.com.dyd.ejemplos.spring.webflux.app.models.documents.Producto;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SpringBootWebfluxApirestApplicationTests {
+	
+	@Autowired
+	private WebTestClient client;
 
 	@Test
-	void contextLoads() {
+	public void listarTest() {
+		client.get()
+		.uri("/api/v2/productos")
+		.accept(MediaType.APPLICATION_JSON_UTF8)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+		.expectBodyList(Producto.class)
+		.hasSize(11);
 	}
 
 }
