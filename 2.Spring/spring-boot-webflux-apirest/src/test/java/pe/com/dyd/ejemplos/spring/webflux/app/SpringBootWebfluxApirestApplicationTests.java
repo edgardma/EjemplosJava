6da7fe86f961ae8ebc3c6330809a6f809fb0cf76
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-09-26 22:33:35 -0500

    Programacion Reactiva API REST - Test en modo MOCK

--- 2.Spring/spring-boot-webflux-apirest/src/test/java/pe/com/dyd/ejemplos/spring/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ 2.Spring/spring-boot-webflux-apirest/src/test/java/pe/com/dyd/ejemplos/spring/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import pe.com.dyd.ejemplos.spring.webflux.app.models.documents.Categoria;
@@ -16,6 +19,12 @@ import pe.com.dyd.ejemplos.spring.webflux.app.models.documents.Producto;
 import pe.com.dyd.ejemplos.spring.webflux.app.models.services.ProductoService;
 import reactor.core.publisher.Mono;
 
+//MOCK el servidor
+//@AutoConfigureWebClient
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+
+//Levanta un servidor
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringBootWebfluxApirestApplicationTests {
 	
