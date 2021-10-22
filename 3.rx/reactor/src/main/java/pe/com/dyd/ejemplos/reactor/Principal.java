bb3f2e5d9cedbe247f6d4ecf22c8993834ffa2c4
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-10-21 23:27:23 -0500

    Spring 5 WebFlux - Ejemplo de Merge

--- 3.rx/reactor/src/main/java/pe/com/dyd/ejemplos/reactor/Principal.java
+++ 3.rx/reactor/src/main/java/pe/com/dyd/ejemplos/reactor/Principal.java
@@ -1,13 +1,45 @@
 package pe.com.dyd.ejemplos.reactor;
 
+import java.time.Duration;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		ejemploMono();
-		ejemploFlux();
+		ejemploMono3();
+		//ejemploMono2();
+		//ejemploMono();
+		//ejemploFlux();
+	}
+	
+	private static void ejemploMono3() {
+		Mono<String> miMono = Mono.justOrEmpty("Hola 1").delayElement(Duration.ofSeconds(2));
+		Mono<String> miMono2 = Mono.justOrEmpty("Hola 2").delayElement(Duration.ofSeconds(3));
+		
+		Flux<String> combinar = Flux.merge(miMono, miMono2);
+		combinar.subscribe(System.out::println);
+		
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void ejemploMono2() {
+		Mono<String> miMono = Mono.justOrEmpty("Hola 1").delayElement(Duration.ofSeconds(2));
+		miMono.subscribe(System.out::println);
+		
+		Mono<String> miMono2 = Mono.justOrEmpty("Hola 2").delayElement(Duration.ofSeconds(3));
+		miMono2.subscribe(System.out::println);
+		
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private static void ejemploMono() {
