f4238f4d75d8908fdb9eba70ed54f9fc79aebf17
Edgard M. Marquez Aylas <edgardma@gmail.com>
2021-12-11 18:24:42 -0500

    Ejemplo de Patron CircuitBreaker

---
+++ 1.Patrones/circuitBreaker/src/main/java/pe/com/dyd/patrones/circuitbreaker/Resilience4jApplication.java
@@ -0,0 +1,11 @@
+package pe.com.dyd.patrones.circuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Resilience4jApplication.class, args);
+    }
+}
