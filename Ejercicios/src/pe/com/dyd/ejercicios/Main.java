b9b967c93be5e91dc70be39f91719d243936aa87
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-08-16 21:11:17 -0500

    Desarrollar el Challenge1

--- Ejercicios/src/pe/com/dyd/ejercicios/Main.java
+++ Ejercicios/src/pe/com/dyd/ejercicios/Main.java
@@ -3,7 +3,10 @@ package pe.com.dyd.ejercicios;
 public class Main {
 
 	public static void main(String[] args) {
-		Challenge0.fissBuzz();
+		//Challenge0.fissBuzz();
+		System.out.println(Challenge1.isAnagram("Prueba", "prueba"));
+		System.out.println(Challenge1.isAnagram("Roma", "amor"));
+		System.out.println(Challenge1.isAnagram("Roma", "amor2"));
 	}
 
 }
