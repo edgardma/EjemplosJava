26745cc5ad3fcbe2f21a89364f6507e9addeca23
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-08-19 09:58:08 -0500

    Desarrollo de Challenge 2

--- Ejercicios/src/pe/com/dyd/ejercicios/Main.java
+++ Ejercicios/src/pe/com/dyd/ejercicios/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		//Challenge0.fissBuzz();
-		System.out.println(Challenge1.isAnagram("Prueba", "prueba"));
-		System.out.println(Challenge1.isAnagram("Roma", "amor"));
-		System.out.println(Challenge1.isAnagram("Roma", "amor2"));
+		
+		//System.out.println(Challenge1.isAnagram("Prueba", "prueba"));
+		//System.out.println(Challenge1.isAnagram("Roma", "amor"));
+		//System.out.println(Challenge1.isAnagram("Roma", "amor2"));
+		
+		Challenge2.fibonacci();
 	}
 
 }
